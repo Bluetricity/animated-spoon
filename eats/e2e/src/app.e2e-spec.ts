0a79f3639ff6a23ffc36ce02534a452560972ea1
tharvey427 <tkierra@gmail.com>
2019-08-27 15:40:04 -0400

    sorry another fix
    accidentally created angular app in git bash, didn't create correctly

--- eats/e2e/src/app.e2e-spec.ts
+++ eats/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to eat!');
+    expect(page.getTitleText()).toEqual('Welcome to eats!');
   });
 
   afterEach(async () => {
--- eats/src/app/app.component.spec.ts
+++ eats/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'eat'`, () => {
+  it(`should have as title 'eats'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('eat');
+    expect(app.title).toEqual('eats');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to eat!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to eats!');
   });
 });
