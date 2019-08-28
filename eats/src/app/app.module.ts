019bf29c95a8c77330107c4313710a80d03b2a8a
tharvey427 <tkierra@gmail.com>
2019-08-27 21:19:49 -0400

    app updates
    added navbar and login component for basic start

--- eats/src/app/app.module.ts
+++ eats/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
