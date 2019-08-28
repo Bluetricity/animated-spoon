2f34994096800916b7183722726d2922704d6361
Adam Hemmelgarn <adamhemmelgarn1995@yahoo.com>
2019-08-28 14:26:07 -0400

    Angular components created for customer pages

--- eats/src/app/app-routing.module.ts
+++ eats/src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { MenuComponent} from './menu/menu.component';
+import { MenuItemsComponent } from './menu-items/menu-items.component';
+import { OrderComponent } from './order/order.component';
+import { DeliveryStatusComponent } from './delivery-status/delivery-status.component'; 
 
 
 const routes: Routes = [
@@ -12,6 +17,27 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'create-account',
+    component: CreateAccountComponent
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
+    /**add later */
+  },
+  {
+    path: 'menu-items',
+    component: MenuItemsComponent
+  },
+  {
+    path: 'order',
+    component: OrderComponent
+  },
+  {
+    path: 'delivery-status',
+    component: DeliveryStatusComponent
   }
 ];
 
