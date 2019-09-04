a939f259f77e75122c4480add975403c2431fbe0
Josh Pontipiedra <joshpontipiedra@gmail.com>
2019-09-03 20:02:50 -0400

    Inventory table shows DB data

---
+++ eats/src/app/get-stock.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { UrlService } from './shared/url.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Stock } from './shared/stock';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetStockService {
+
+  private appUrl = this.caUrl.getUrl() + '/stock/';
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private caUrl: UrlService, private http: HttpClient) { }
+
+  make(): Observable<Stock[]>{
+    return this.http.get(this.appUrl, {withCredentials: true}).pipe(
+      map( resp => resp as Stock[])
+    );
+  }
+  // make(ingredient: string, quantity: number){
+  //   const obj = {ingredient:ingredient, quantity:quantity};
+  //   const body = JSON.stringify(obj);
+
+  //   return this.http.get(this.appUrl, {headers: this.headers, withCredentials: true}).pipe(
+  //     map(resp =>{
+  //       const currStock: Stock = resp as Stock;
+  //       return currStock;
+  //     })
+  //   );
+  // }
+}
--- eats/src/app/inventory/inventory.component.ts
+++ eats/src/app/inventory/inventory.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Stock } from '../shared/stock';
 import { Router } from '@angular/router';
-
+import { GetStockService } from '../get-stock.service';
 import { Observable } from 'rxjs';
 import sampleData from '../data.json';
 
@@ -17,13 +17,23 @@ export class InventoryComponent implements OnInit {
   columns: string[];
   test: string[];
 
+  public ingredient: string;
+  public quantity: number;
+  public getStock: Stock;
+  public currStock: Stock[];
+
   //@Input() stock: Stock;
 
   Inv: any = sampleData
+  stocks: Stock[];
 
-  constructor(private router: Router) { }
+  constructor(private createService: GetStockService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.createService.make().subscribe((data: Stock[]) => {
+      this.stocks = data;
+    });
   }
   backToHome(){
     this.router.navigate(['home']);
@@ -31,4 +41,16 @@ export class InventoryComponent implements OnInit {
   addNewItem(){
     this.router.navigate(['new-stock']);
   }
+  getItem(){
+    // this.createService.make(this.ingredient,this.quantity).subscribe(
+    //   resp =>{
+    //     this.getStock = resp;
+    //   }
+    // )
+    this.createService.make().subscribe(stock => this.currStock = stock);
+    
+
+  }
 }
+
+
